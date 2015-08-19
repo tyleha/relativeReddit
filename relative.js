d2458fe186d8dcc5fa8dcd83c343d18965db92bf
Tyler Hartley <tyleha@gmail.com>
2015-08-19 00:43:25 -0700

    Add injected badge and cleanup code.

--- relative.js
+++ relative.js
@@ -1,12 +1,39 @@
-// https://carl-topham.com/theblog/post/creating-chrome-extension-uses-jquery-manipulate-dom-page/
-// http://stackoverflow.com/questions/14068879/modify-html-of-loaded-pages-using-chrome-extensions
-
+///////////////
+// Constants //
+///////////////
 //https://vis4.net/labs/multihue/#colors=white, yellow, deeppink, darkred|steps=25|bez=1|coL=1
-const POSSIBLE_COLORS = ['#ffffff', '#fff8c6', '#ffedac', '#ffe29b', '#ffd88f', '#ffcb86', '#ffbf7f', '#ffb379', '#fda873', '#fa9c6f', '#f6916a', '#f28666', '#ed7b62', '#e86f5e', '#e26459', '#dc5954', '#d64e4f', '#ce4349', '#c63842', '#bf2d3a', '#b52232', '#ac1729', '#a20c1f', '#970311', '#8b0000']
-
-
+var POSSIBLE_COLORS = ['#ffffff', '#fff8c6', '#ffedac', '#ffe29b', '#ffd88f', '#ffcb86', '#ffbf7f', '#ffb379', '#fda873', '#fa9c6f', '#f6916a', '#f28666', '#ed7b62', '#e86f5e', '#e26459', '#dc5954', '#d64e4f', '#ce4349', '#c63842', '#bf2d3a', '#b52232', '#ac1729', '#a20c1f', '#970311', '#8b0000'];
 var ASSUMED_RATIO = 0.6;
 var MAX_RATIO_POSSIBLE = 4;
+
+var pointRegex = new RegExp(/(\d+)/);
+
+var hoverDescriptions = [
+  {value: -Infinity, text: 'Either boring or new'},
+  {value: 0.3, text: 'An ordinary comment'},
+  {value: ASSUMED_RATIO, text: 'Above average and worth reading'},
+  {value: 1, text: 'Better than its parent comment'},
+  {value: 1.5, text: 'A real zinger'},
+  {value: 2, text: 'Brilliant stuff'},
+  {value: 2.5, text: 'Transcendent. Call your Mom.'},
+  {value: Infinity, text: ''},
+];
+
+///////////////
+// Functions //
+///////////////
+function hoverText (ratio) {
+  var idx = 0;
+  var current = hoverDescriptions[0].value;
+  var text = hoverDescriptions[0].text;
+  while (ratio > current) {
+    text = hoverDescriptions[idx].text;
+
+    idx += 1;
+    current = hoverDescriptions[idx].value;
+  }
+  return text;
+}
 
 /** Convert a number [0 1] to a color in  an array */
 function fractionToColor (properFraction) {
@@ -16,11 +43,9 @@ function fractionToColor (properFraction) {
     return POSSIBLE_COLORS[POSSIBLE_COLORS.length-1]; //last element
   }
   var idx = Math.floor(POSSIBLE_COLORS.length*properFraction);
-  console.log('idx', idx)
   return POSSIBLE_COLORS[idx];
 }
 
-var pointRegex = new RegExp(/(\d+)/);
 function parseScore(text) {
   return pointRegex.exec(text)[1]; //first match
 }
@@ -30,50 +55,47 @@ function logMe(val) {
 }
 
 function getRatioColor(ratio, rawScore) {
-
-  console.log('ratio', ratio)
   var decreasePowerBelow = 12
   if (rawScore < decreasePowerBelow) {
     ratio = ratio * rawScore/decreasePowerBelow;
-    console.log('new ratio for score '+rawScore, ratio)
   }
   // map to 0 to 1 (expected to be in -MAX_RATIO_POSSIBLE to MAX_RATIO_POSSIBLE range)
-  normScore = (ratio - ASSUMED_RATIO) / MAX_RATIO_POSSIBLE; // number of times over expected ratio norm'd
+  // number of times over expected ratio norm'd
+  var normScore = (ratio - ASSUMED_RATIO) / MAX_RATIO_POSSIBLE;
 
-  console.log('normScore', normScore)
   return fractionToColor(normScore);
 }
 
 //////////////////////
 // Application Code //
 //////////////////////
-
 var allParentChains = $('.sitetable.nestedlisting').first().find('> .comment');
 
+// For each top-level comment
 allParentChains.each(function() {
 
+  // For each subcomment for this top-level comment
   $(this).find('.score.unvoted').each(function(elem) {
-    t = $(this);
-    // p = $(this).closest('.comment')
-    p = $(this).parents('.comment').eq(1);
-    p2 = p.find('.score.unvoted').first();
-
+    var t = $(this);
+    // Find this comment's parent
+    var parent = $(this).parents('.comment').eq(1).find('.score.unvoted').first();
     var thisScore = parseScore(t.text());
-    if (p2.text()) {
-      var parentScore = parseScore(p2.text());
+
+    // If we can find a parent score (i.e. aren't the first in a chain)
+    if (parent.text()) {
+      var parentScore = parseScore(parent.text());
       if (parentScore < 1) {
         parentScore = 1; //roughly handle really low vote counts
       }
+      // Compute our ratio
       var ratio = thisScore/parentScore;
-
-
+      // Deterime what color, if any, to show
       var color = getRatioColor(ratio, thisScore);
-      console.log('selecting color', color)
-
-      var newbie = t.after('<span>'+ ratio.toFixed(2) +'</span>');
-      newbie.css('background-color', color);
+      // Inject our badge.
+      $('<span>'+ ratio.toFixed(1) +'x</span>').insertAfter(t).css({
+        'background-color': color,
+        'cursor': 'default',
+      }).addClass('relative-tag').attr('title', hoverText(ratio));
     }
-
   });
-
 });
