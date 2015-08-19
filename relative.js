57015a4a8360a0aadf228824840e3975b2a2b73b
Tyler Hartley <tyleha@gmail.com>
2015-08-18 22:05:52 -0700

    Move to set number of color scheme.

--- relative.js
+++ relative.js
@@ -2,38 +2,18 @@
 // http://stackoverflow.com/questions/14068879/modify-html-of-loaded-pages-using-chrome-extensions
 
 //https://vis4.net/labs/multihue/#colors=white, yellow, deeppink, darkred|steps=25|bez=1|coL=1
-['#ffffff', '#fff8c6', '#ffedac', '#ffe29b', '#ffd88f', '#ffcb86', '#ffbf7f', '#ffb379', '#fda873', '#fa9c6f', '#f6916a', '#f28666', '#ed7b62', '#e86f5e', '#e26459', '#dc5954', '#d64e4f', '#ce4349', '#c63842', '#bf2d3a', '#b52232', '#ac1729', '#a20c1f', '#970311', '#8b0000']
+const POSSIBLE_COLORS = ['#ffffff', '#fff8c6', '#ffedac', '#ffe29b', '#ffd88f', '#ffcb86', '#ffbf7f', '#ffb379', '#fda873', '#fa9c6f', '#f6916a', '#f28666', '#ed7b62', '#e86f5e', '#e26459', '#dc5954', '#d64e4f', '#ce4349', '#c63842', '#bf2d3a', '#b52232', '#ac1729', '#a20c1f', '#970311', '#8b0000']
 
-var hsv2rgb = function(val, opacity) {
-  // adapted from http://schinckel.net/2012/01/10/hsv-to-rgb-in-javascript/
-  var h = Math.floor(val * 120);
-  var s = val;
-  var v = 1;
-  var rgb, i, data = [];
-  opacity = opacity || 1;
-
-  // Ignore if below zero. (make white).
-  if (s <= 0) {
-    rgb = [v,v,v];
-  } else {
-    h = h / 120;
-
-    data = [v*(1-s), v*(1-s*(h)), v*(1-s*(1-(h)))];
-    rgb = [v, data[1], data[0]];
-
-  }
-  return 'rgba('+rgb.map(function(d) {
-    return Math.round(d*255)
-  }).join(', ') + ', ' + opacity + ')';
-};
-
-function whiteToRed(val) {
-  var reddness = Math.floor(val * 255);
-  return 'rgba(' + reddness + ', 0, 0, 1)';
-}
 
 const ASSUMED_RATIO = 0.5;
 const ASSUMED_LOG = Math.log(1/ASSUMED_RATIO);
+
+/** Convert a number [0 1] to a color in  an array */
+function fractionToColor (properFraction) {
+  var idx = Math.floor(POSSIBLE_COLORS.length*properFraction);
+  console.log('idx', idx)
+  return POSSIBLE_COLORS[idx];
+}
 
 var pointRegex = new RegExp(/(\d+)/);
 function parseScore(text) {
@@ -44,14 +24,14 @@ function logMe(val) {
   return Math.log(val) / ASSUMED_LOG;
 }
 
-function whatColorAmI(ratio) {
+function getRatioColor(ratio) {
 
   console.log('ratio', ratio)
   var numTimesOverExpected = logMe(ratio / ASSUMED_RATIO);
   // map to 0 to 1 (expected to be in -2 to 2 range)
-  var _sc = numTimesOverExpected / 3;
-  console.log('sc', _sc)
-  return hsv2rgb(_sc, 0.5);
+  var normScore = numTimesOverExpected / 3;
+  console.log('normScore', normScore)
+  return fractionToColor(normScore);
 }
 
 //////////////////////
@@ -74,7 +54,9 @@ allParentChains.each(function() {
       var ratio = thisScore/parentScore;
 
 
-      var color = whatColorAmI(ratio);
+      var color = getRatioColor(ratio);
+      console.log('selecting color', color)
+
       var newbie = t.after('<span>'+ ratio.toFixed(2) +'</span>');
       newbie.css('background-color', color);
     }
@@ -82,10 +64,3 @@ allParentChains.each(function() {
   });
 
 });
-
-
-// var score = $('.comment').first().find('.score.unvoted').first().text()
-
-// var $score = $('.comment').first().find('.score.unvoted').first()
-// $score.append(color + ' ' + parseScore(score));
-// $score.css('background-color', color);
